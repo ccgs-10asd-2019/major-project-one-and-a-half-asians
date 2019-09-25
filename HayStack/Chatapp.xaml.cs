3d6d72c87c28e9dd19ba04a888d53c6529c2b133
toddeee <1011877@student.ccgs.wa.edu.au>
2019-09-25 11:09:10 +0800

    Add new chat

--- HayStack/Chatapp.xaml.cs
+++ HayStack/Chatapp.xaml.cs
@@ -66,7 +66,7 @@ namespace HayStack
                 {
                     
                     TcpClient tcpClient = new TcpClient();
-                    tcpClient.Connect("10.60.253.110", 4000);
+                    tcpClient.Connect("10.60.250.150", 4000);
                     serverStream = tcpClient.GetStream();
                     AddPrompt();
                     byte[] outStream = Encoding.ASCII.GetBytes(txtChatName.Text.Trim() + " is reconnecting");
@@ -76,7 +76,7 @@ namespace HayStack
                 }
                 else
                 {
-                    tcpClient.Connect("10.60.253.110", 4000);
+                    tcpClient.Connect("10.60.250.150", 4000);
                     serverStream = tcpClient.GetStream();
                     AddPrompt();
                     byte[] outStream = Encoding.ASCII.GetBytes(txtChatName.Text.Trim() + " is joining");
--- HayStack/MainWindow.xaml.cs
+++ HayStack/MainWindow.xaml.cs
@@ -31,11 +31,11 @@ namespace Hey_Stack_Project
 
         private void ChatAppBtnClick(object sender, RoutedEventArgs e)
         {
-            ShowMyChatApp();
-            //Process p = new Process();
+            //ShowMyChatApp();
+            Process p = new Process();
             //need to figure this part out
-            //p.StartInfo = new ProcessStartInfo("C:\\Users\\isaac\\Documents\\Coding\\Major Project App\\ChatClient\\bin\\Debug\\ChatterClient.exe");
-            //p.Start();
+            p.StartInfo = new ProcessStartInfo("C:\\User Program Files\\Advanced Software Visual\\Hay\\ChatterClient\\bin\\Debug\\ChatterClient.exe");
+            p.Start();
 
         }
 
